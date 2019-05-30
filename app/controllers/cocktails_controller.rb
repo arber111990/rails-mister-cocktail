5afbdc90eef98a135ca8202c80e671d229d62a66
Arber Zyb <arber111990@gmail.com>
2019-05-30 12:20:53 +0200

    rails new

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,27 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktails = Cocktail.find(params[:id])
+    @dose = Dose.new
+    @review = Review.new
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+     @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render 'new'
+    end
+
+  def edit
+    @cocktails = Cocktail.find(params[:id])
+  end
+end
